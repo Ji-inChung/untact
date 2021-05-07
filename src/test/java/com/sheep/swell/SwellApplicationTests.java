da56e4c23415f6ba2319f4b8337da3d2f8101ff9
Ji-inChung <bluesheep7@gmail.com>
2021-05-07 23:15:58 +0900

    first commit

---
+++ src/test/java/com/sheep/swell/SwellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheep.swell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
